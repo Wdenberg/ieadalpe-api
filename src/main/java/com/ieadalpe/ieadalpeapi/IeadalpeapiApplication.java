d69ab7c61b117887f4e3b2624df74e890c0dc832
wdenberg <wdenbergskillshd@hotmail.com>
2026-04-11 21:26:48 -0300

    Initial commit: Projeto Java configurado

---
+++ src/main/java/com/ieadalpe/ieadalpeapi/IeadalpeapiApplication.java
@@ -0,0 +1,13 @@
+package com.ieadalpe.ieadalpeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeadalpeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeadalpeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ieadalpe/ieadalpeapi/IeadalpeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieadalpe.ieadalpeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeadalpeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
